d48becad708b3c8a63911a262a2c186b0c8a6869
Eduardo <ehernandezvilla@gmail.com>
2023-09-19 22:16:35 -0300

    code: adding dotenv

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config();
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+
 
 const app = express()
 
